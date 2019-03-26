f4b9661298bdf4dd3bf0f651e1186a7d03c04d9a
Akinoe <fellowsdylan16@gmail.com>
2019-03-26 08:53:22 -0500

    Final Commit

--- SpringBootDemo/src/main/java/com/tek/vehicle/VehicleApplication.java
+++ SpringBootDemo/src/main/java/com/tek/vehicle/VehicleApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// THIS ANNOTATION MUST BE PRESENT TO ALLOW THE CREATED AT AND LAST UPDATED
-// DATE TO BE AUTOMATICALLY POPULATED.
 @EnableJpaAuditing
 
-// LETS US KNOW THAT THIS IS THE CLASS THAT STARTS UP THE SPRING BOOT APP
 @SpringBootApplication
 public class VehicleApplication extends SpringBootServletInitializer {
 	
